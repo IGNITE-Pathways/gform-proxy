a59dcab4b9f6facde079072b30cbcb7f2133f272
Rajeev <rajeev@cowbellcyber.ai>
2025-05-04 17:00:38 -0700

    moved Forms to Ignite

--- api/googleform.js
+++ api/googleform.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbxoMlmjuxxo_zK9iYdGbCoFfKZRa62tGa0LPJWuzQIjcWzePbCws88aVVL4RAmmcrqy/exec', {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbx6IJF42v2Lp3gOX4l5ZvlAZsUn9w7twLgBGWoLCrngENdY4NwXw-zg-ibLyTzG2b0I/exec', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(req.body),
